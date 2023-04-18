f8367310ec2ea8f389fb41f876a985733091fa4d
yudai64 <kemurin.yudai0723@gmail.com>
2023-04-19 08:00:19 +0900

    submit 183

---
+++ solutions/183/183.customers-who-never-order.sql
@@ -0,0 +1,13 @@
+--
+-- @lc app=leetcode id=183 lang=mysql
+--
+-- [183] Customers Who Never Order
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT c.name AS Customers
+FROM Customers c LEFT JOIN Orders o ON c.id = o.customerId
+WHERE o.id IS NULL
+-- @lc code=end
+
