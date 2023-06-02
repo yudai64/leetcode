aa14fa7efc64e0f8dfbbcae493bd485cce3dd7cb
yudai64 <kemurin.yudai0723@gmail.com>
2023-06-03 08:22:42 +0900

    submit 412

---
+++ solutions/412/412.fizz-buzz.go
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=412 lang=golang
+ *
+ * [412] Fizz Buzz
+ */
+
+// @lc code=start
+func fizzBuzz(n int) []string {
+	var ans []string
+	for i := 1; i <= n; i++ {
+		if i%15 == 0 {
+			ans = append(ans, "FizzBuzz")
+		} else if i%3 == 0 {
+			ans = append(ans, "Fizz")
+		} else if i%5 == 0 {
+			ans = append(ans, "Buzz")
+		} else {
+			ans = append(ans, strconv.Itoa(i))
+		}
+	}
+	return ans
+}
+
+// @lc code=end
+
